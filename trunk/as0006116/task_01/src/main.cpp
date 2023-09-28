3b5a5c8a998ab79a21c16bae3053d7ec368e714c
DopplerCore <110098650+DopplerCore@users.noreply.github.com>
2023-09-28 23:05:36 +0300

    CMake added

--- trunk/as0006116/task_01/src/main.cpp
+++ trunk/as0006116/task_01/src/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
+	//Константы
     const double a = 1.1;
     const double b = 0.03;
     const double c = 0.7;
@@ -10,24 +11,22 @@ int main()
     double Uwarm = 0;
     double Ytemp_1 = 17;
     double Uwarm_1 = 19;
+	 //ВВод данных
     cout << "Input temperature: ";
     cin >> Ytemp;
-    cout << "\nInput warm: ";
+    cout << "Input warm: ";
     cin >> Uwarm;
-    cout << "\n";
-    double y[15] = { 0 };
+	 double y[15] = { 0 };
     y[0] = Ytemp_1;
     y[1] = Ytemp;
-    for (int i = 2; i < 15; i++) {
-        y[i] = a * y[i - 1] - b * pow(y[i - 2], 2) + c * Uwarm + d * sin(Uwarm_1);
-    }
-    cout << "For nonlinear model:\n";
-    for (int i = 0; i < 15; i++) cout << "y["<< i <<"] = " << y[i] << " \n";
-    cout << "\n";
-    for (int i = 2; i < 15; i++) {
-        y[i] = a * y[i - 1] + b * Uwarm;
-    }
+	 //Вычисления по формуле линейной модели
+	 for (int i = 2; i < 15; i++) { y[i] = a * y[i - 1] + b * Uwarm; }
     cout << "For linear model:\n";
     for (int i = 0; i < 15; i++) cout << "y[" << i << "] = " << y[i] << " \n";
-    return 0;
+	 cout << "\n";
+	 //Вычисления по формуле нелинейной модели
+    for (int i = 2; i < 15; i++) { y[i] = a * y[i - 1] - b * pow(y[i - 2], 2) + c * Uwarm + d * sin(Uwarm_1); }
+    cout << "For nonlinear model:\n";
+    for (int i = 0; i < 15; i++) cout << "y["<< i <<"] = " << y[i] << " \n";
+	 return 0;
 }
