efb2d50276b7a82483d5f28e54ade804d3895851
Василенко Илья <142149109+wasilenkai@users.noreply.github.com>
2023-10-03 00:45:01 +0300

    Update main.cpp

--- trunk/as0006102/task_01/src/main.cpp
+++ trunk/as0006102/task_01/src/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
 //линейная модель
-void linear(double a, double b, double c, double d, double uT, double yT, int t) {
+void linear(double a, double b, double uT, double yT, int t) {
     cout << "\t---Temperature modelling using a linear model---" << endl;
     for (int i = 1; i <= t; i++) {
         double yT1 = a * yT + b * uT;
@@ -22,16 +21,19 @@ void nonLinear(double a, double b, double c, double d, double uT, double yT, int
     cout << "\t---Temperature modelling using a NON-linear model---" << endl;
     for (int i = 1; i <= t; i++) {
         double yT1 = a * yT - b * pow(yT_1, 2) + c * uT + d * sin(uT_1);
-        cout << "y" << i << " = "  << yT1 << endl;
+        cout << "y" << i << " = " << yT1 << endl;
 
         uT_1 = uT;
         yT_1 = yT;
         yT = yT1;
-    }   
+    }
 }
 
-int main(){
-    double a, b, c, d; //константы
+int main() {
+    double a; //константа
+    double b; //константа
+    double c; //константа
+    double d; //константа
     double uT; //теплота
     double yT; //температура
     int t; // кол-во дискретных моментов времени
@@ -40,12 +42,12 @@ int main(){
     cout << "Enter the constant b "; cin >> b;
     cout << "Enter the constant c "; cin >> c;
     cout << "Enter the constant d "; cin >> d;
-    cout << "Enter the warm"; cin >> uT;
-    cout << "Enter the  temperature "; cin >> yT; 
+    cout << "Enter the warm "; cin >> uT;
+    cout << "Enter the  temperature "; cin >> yT;
     cout << "Enter the number of discrete times "; cin >> t;
-    
-    linear(a, b, c, d, uT, yT, t);
-    nonLinear(a,  b, c, d, uT, yT, t);
+
+    linear(a, b, uT, yT, t);
+    nonLinear(a, b, c, d, uT, yT, t);
 
     return 0;
 }
