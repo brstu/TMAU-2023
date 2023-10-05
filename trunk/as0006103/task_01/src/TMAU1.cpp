3ec118f686cfd758bb2a03d56c3245c62e644028
marla124 <95192268+marla124@users.noreply.github.com>
2023-10-05 21:36:47 +0300

    Update TMAU1.cpp

--- trunk/as0006103/task_01/src/TMAU1.cpp
+++ trunk/as0006103/task_01/src/TMAU1.cpp
@@ -1,61 +1,97 @@
 ﻿#include <iostream>
 #include <cmath>
+template<typename Type>
 
-double NotModelLiner(double a, double b, double yt, double ut, double prev_ut, double prev_yt, double c, double d)
-{
-    return a * yt - b * pow(prev_yt, 2) + c * ut + d * sin(prev_ut);
 
-}
-double ModelLiner(double a, double b, double yt, double ut)
-{
-    return a * yt + b * ut;
-
-}
-void EnterValue(double &value, std::string nameValue)
+void EnterValue(Type& value, std::string nameValue)
 {
     std::cout << nameValue;
     std::cin >> value;
 }
-void OutputModel(double y, double u, double NumLin, double NotNumLin, double aLiner, double bLiner, double PrevUt, double PrevYt, double aNotLiner, double bNotLiner, double c, double d)
+class Object {
+public:
+    virtual ~Object() = default;
+    virtual double ModelFunction(double Yt, double Ut) = 0;
+};
+class NotModelLiner : Object
 {
-    double Yt = y, Ut = u;
-    std::cout << std::endl <<std::endl<< "Liner model";
-    std::cout << "\t\t\tIteration number:" << "\t\t\t" << "Yt";
-    for (int i = 0; i < NumLin; i++) {
-        std::cout << std::endl;
-        EnterValue(Ut, "enter value Ut:");
-
-        Yt = ModelLiner(aLiner, bLiner, Yt, Ut);
-        std::cout << "\t\t\t\t" << i + 1 << "\t\t\t\t"<<Yt;
+public:
+    double a;
+    double b;
+    double prev_ut = 0;
+    double prev_yt = 0;
+    double c;
+    double d;
+    NotModelLiner(double a, double b, double c, double d) {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+        this->d = d;
     }
+    ~NotModelLiner() = default;
+    double ModelFunction(double yt, double ut) {
+        return a * yt - b * pow(prev_yt, 2) + c * ut + d * sin(prev_ut);
+    }
+    void OutputModel(double y, double u, int NotNumLin)
+    {
+        double Yt = y, Ut = u;
 
+        std::cout << std::endl << std::endl << "Not liner model";
+        std::cout << "\t\t\tIteration number:" << "\t\t\t" << "Yt";
+        for (int i = 0; i < NotNumLin; i++) {
+            if (i > 0) {
+                prev_ut = Ut, prev_yt = Yt;
+                std::cout << std::endl;
+                EnterValue(Ut, "enter value Ut:");
+                Yt = ModelFunction(Yt, Ut);
+                std::cout << "\t\t\t\t" << i + 1 << "\t\t\t\t" << Yt;
+            }
+            else {
+                prev_ut = 0; prev_yt = 0;
+                std::cout << std::endl;
+                EnterValue(Ut, "enter value Ut:");
+                Yt = ModelFunction(Yt, Ut);
+                std::cout << "\t\t\t\t" << i + 1 << "\t\t\t\t" << Yt;
+            }
+        }
 
-    std::cout << std::endl << std::endl<<"Not liner model";
-    std::cout << "\t\t\tIteration number:" << "\t\t\t" << "Yt";
-    for (int i = 0; i < NotNumLin; i++) {
-        if (i > 0) {
-            PrevUt = Ut, PrevUt = Yt;
+    }
+};
+
+class ModelLiner : Object
+{
+public:
+    double a;
+    double b;
+    ModelLiner(double a, double b) {
+        this->a = a;
+        this->b = b;
+    }
+    ~ModelLiner() = default;
+    double ModelFunction(double yt, double ut) {
+        return a * yt + b * ut;
+    }
+    void OutputModel(double y, double u, int NumLin)
+    {
+        double Yt = y, Ut = u;
+        std::cout << std::endl << std::endl << "Liner model";
+        std::cout << "\t\t\tIteration number:" << "\t\t\t" << "Yt";
+        for (int i = 0; i < NumLin; i++) {
             std::cout << std::endl;
             EnterValue(Ut, "enter value Ut:");
-            Yt = NotModelLiner(aNotLiner, bNotLiner, Yt, Ut, PrevUt, PrevYt, c, d);
-            std::cout << "\t\t\t\t" << i + 1 << "\t\t\t\t" << Yt;
-        }
-        else {
-            PrevUt = 0; PrevYt = 0;
-            std::cout << std::endl;
-            EnterValue(Ut, "enter value Ut:");
-            Yt = NotModelLiner(aNotLiner, bNotLiner, Yt, Ut, PrevUt, PrevYt, c, d);
+
+            Yt = ModelFunction(Yt, Ut);
             std::cout << "\t\t\t\t" << i + 1 << "\t\t\t\t" << Yt;
         }
     }
+};
 
-}
 
 int main()
 {
-    double y=0, u=0, bLiner=0,aLiner=0,bNotLiner=0,aNotLiner=0, c=0, d=0, NumLin=0,NumNotLin=0, PrevUt=0, PrevYt=0;
-
-    std::cout << "Number of iteration"<<std::endl;
+    double y = 0, u = 0, bLiner = 0, aLiner = 0, bNotLiner = 0, aNotLiner = 0, c = 0, d = 0;
+    int NumLin = 0, NumNotLin = 0;
+    std::cout << "Number of iteration" << std::endl;
     EnterValue(NumLin, "Liner Model:");
     EnterValue(NumNotLin, "Not Liner Model:");
 
@@ -72,5 +108,9 @@ int main()
     EnterValue(c, "enter value c:");
     EnterValue(d, "enter value d:");
 
-    OutputModel(y, u, NumLin, NumNotLin, aLiner, bLiner, PrevUt, PrevYt, aNotLiner, bNotLiner, c, d);
+    ModelLiner modelLin(aLiner, bLiner);
+    modelLin.OutputModel(y, u, NumLin);
+    NotModelLiner notmodelLin(aNotLiner, bNotLiner, c, d);
+    notmodelLin.OutputModel(y, u, NumLin);
+
 }
