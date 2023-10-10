e2433bc26570f3d45999c2afee21bf95dd3d7968
Ляшенко Михаил Валерьевич <as006215@g.bstu.by>
2023-10-10 21:41:39 +0300

    solution_468213

--- trunk/as0006213/task_01/src/ConsoleApplication1.cpp
+++ trunk/as0006213/task_01/src/ConsoleApplication1.cpp
@@ -13,24 +13,24 @@ private:
     std::vector <double> u;
     std::stack <double> y;
 public:
-    explicit Model(std::vector <double> &u) {
+    explicit Model(const std::vector <double>& u) {
         setConstants(0, 0, 0, 0);
         y.push(20.0);
         y.push(20.0);
         setU(u);
-    };
+    }
     void setU(std::vector <double> u) {
         u.push_back(20.0);
-        for (int i = 0; i < u.size(); i++) {
+        for (int i : u) {
             this->u.push_back(u[i]);
         }
-    };
-    void setConstants(double a, double b, double c, double d) {
-        this->a = a;
-        this->b = b;
-        this->c = c;
-        this->d = d;
-    };
+    }
+    void setConstants(double newA, double newB, double newC, double newD) {
+        this->a = newA;
+        this->b = newB;
+        this->c = newC;
+        this->d = newD;
+    }
     void stepModelingNL(int i) {
         i += 1;
         double yPrev = y.top();
@@ -38,12 +38,14 @@ public:
         double yPPrev = y.top();
         y.push(yPrev);
         y.push(a * yPrev - b * pow(yPPrev, 2) + c * u[i] + d * sin(u[i - 1]));
-    };
+    }
     void stepModelingL(int i) {
         double yNext = a * y.top() - b * u[i];
         y.push(yNext);
-    };
-    double getY() {return y.top(); };
+    }
+    double getY() { 
+        return y.top(); 
+    }
 };
 
 int main()
@@ -54,7 +56,7 @@ int main()
     for (int i = 0; i < num; i++) {
         u.push_back(20.0 - i / 5.0 + i * i / 9.0 - i * i * i / 260.0);
         std::cout << u[i] << "\t";
-    };
+    }
     std::cout << std::endl;
     Model model1(u);
     //Нелинейная модель
@@ -62,13 +64,13 @@ int main()
     for (int i = 0; i < u.size(); i++) {
         model1.stepModelingNL(i);
         std::cout << model1.getY() << "\t";
-    };
+    }
     std::cout << std::endl;
     //Линейная модель
     Model model2(u);
-    model2.setConstants(1.15, 0.133, 0, 0); //Набор констант подобран вручную
+    model2.setConstants(1.15, 0.1115, 0, 0); //Набор констант подобран вручную
     for (int i = 0; i < u.size(); i++) {
         model2.stepModelingL(i);
         std::cout << model2.getY() << "\t";
-    };
+    }
 };
