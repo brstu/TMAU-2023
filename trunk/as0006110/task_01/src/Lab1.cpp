8615c0e64b17311ca66a78053462f1516ae479ba
Endorvey <plusheviy06@gmail.com>
2023-10-13 10:26:40 +0300

    up lab1

--- trunk/as0006110/task_01/src/Lab1.cpp
+++ trunk/as0006110/task_01/src/Lab1.cpp
@@ -3,65 +3,62 @@
 
 using namespace std;
 
-#define nm 16		
+#define newmodu 16		
 
 
 
-double nonlinear(double y[nm], double u, double a, double b, double c, double d) {
-	double new_gradus[nm];
-	y[1] = b * u + a * y[0];
-	new_gradus[1] = y[1];
-	for (int i = 2; i < nm; i++) {
-		y[i] = a * y[i - 1] - b * pow(y[i - 2], 2) + d * sin(u) + c * u;
-		new_gradus[i] = y[i];
-	}
-	return new_gradus[nm - 1];
+          double nLin(double y[newmodu], double u, double a, double b, double c, double d) {
+          double ng[newmodu];
+          y[1] = b * u + a * y[0];
+          	ng[1] = y[1];
+				for (int i = 2; i < newmodu; i++) {
+				y[i] = a * y[i - 1] - b * pow(y[i - 2], 2) + d * sin(u) + c * u;
+				ng[i] = y[i];
+				}
+				return ng[newmodu - 1];
 }
 
-double linear(double y[nm], double u, double a, double b) {
-	double new_gradus[nm];
-	for (int i = 1; i < nm; i++) {
-		y[i] = b * u + a * y[i - 1];
-		new_gradus[i] = y[i];
-	}
-	return new_gradus[nm - 1];
-}
-
-
+		double Lin(double y[newmodu], double u, double a, double b) {
+			double ng[newmodu];
+				for (int i = 1; i < newmodu; i++) {
+					y[i] = b * u + a * y[i - 1];
+					ng[i] = y[i];
+					}
+					return ng[newmodu - 1];
+					}
 
 
 int main() {
 
-	double temperature[nm];
-	double choosen_warm;
-	int temp;
+double temperat[newmodu];
+	double chowa;
+		int temp;
 
-	cout << "choose : \n 0 - linear \n 1,2,3,4,5,6,7,8,9 - nonlinear" << endl;
-	cin >> temp;
-	cout << "enter temperature " << endl;
-	cin >> temperature[0];
-	cout << "enter choosen_warm " << endl;
-	cin >> choosen_warm;
+           cout << "choose : \n 0 - Lin \n 1,2,3,4,5,6,7,8,9 - nLin" << endl;
+           cin >> temp;
+           cout << "enter temperat " << endl;
+           cin >> temperat[0];
+           cout << "enter chowa " << endl;
+		   cin >> chowa;
 
 
-	const double z = 0.1;
-	const double x = 0.01;
-	const double c = 0.03;
-	const double v = 0.02;
-	if (temp == 0) {
-		linear(temperature, choosen_warm, z, x);
-		for (int i = 0; i < nm; i++) {
-			cout << "y[" << i << "]=" << temperature[i] << endl;
-		}
-	}
-	else {
-		nonlinear(temperature, choosen_warm, z, x, c, v);
-		for (int i = 0; i < nm; i++) {
-			cout << "y[" << i << "]=" << temperature[i] << endl;
-		}
-	}
+          	const double z = 0.1;
+          	const double x = 0.01;
+          	const double c = 0.03;
+          	const double v = 0.02;
+          if (temp == 0) {
+          Lin(temperat, chowa, z, x);
+          for (int i = 0; i < newmodu; i++) {
+                    cout << "y[" << i << "]=" << temperat[i] << endl;
+          }
+          }
+                    else {
+          nLin(temperat, chowa, z, x, c, v);
+                    for (int i = 0; i < newmodu; i++) {
+                              cout << "y[" << i << "]=" << temperat[i] << endl;
+          }
+                              }
 
-	system("pause");
-	return 0;
+system("pause");
+return 0;
 }
-
