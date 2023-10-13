5a2064a5a302406f6f428e517a0cb882c75354cc
Endorvey <plusheviy06@gmail.com>
2023-10-13 10:50:37 +0300

    up main2

--- trunk/as0006110/task_01/src/main.cpp
+++ trunk/as0006110/task_01/src/main.cpp
@@ -3,34 +3,34 @@
 
 using namespace std;
 
-#define newmodu 16		
 
 
 
-          double nLin(double y[newmodu], double u, double a, double b, double c, double d) {
-          double ng[newmodu];
+
+          double nLin(double y[16], double u, double a, double b, double c, double d) {
+          double ng[16];
           y[1] = b * u + a * y[0];
           	ng[1] = y[1];
-				for (int i = 2; i < newmodu; i++) {
+				for (int i = 2; i < 16; i++) {
 				y[i] = a * y[i - 1] - b * pow(y[i - 2], 2) + d * sin(u) + c * u;
 				ng[i] = y[i];
 				}
-				return ng[newmodu - 1];
+				return ng[15];
 }
 
-		double Lin(double y[newmodu], double u, double a, double b) {
-			double ng[newmodu];
-				for (int i = 1; i < newmodu; i++) {
+		double Lin(double y[16], double u, double a, double b) {
+			double ng[16];
+				for (int i = 1; i < 16; i++) {
 					y[i] = b * u + a * y[i - 1];
 					ng[i] = y[i];
 					}
-					return ng[newmodu - 1];
+					return ng[15];
 					}
 
 
 int main() {
 
-double temperat[newmodu];
+double temperat[16];
 	double chowa;
 		int temp;
 
@@ -48,13 +48,13 @@ double temperat[newmodu];
          double v = 0.02;
           if (temp == 0) {
           Lin(temperat, chowa, z, x);
-          for (int i = 0; i < newmodu; i++) {
+          for (int i = 0; i < 16; i++) {
                     cout << "y[" << i << "]=" << temperat[i] << endl;
           }
           }
                     else {
           nLin(temperat, chowa, z, x, c, v);
-                    for (int i = 0; i < newmodu; i++) {
+                    for (int i = 0; i < 16; i++) {
                               cout << "y[" << i << "]=" << temperat[i] << endl;
           }
                               }
