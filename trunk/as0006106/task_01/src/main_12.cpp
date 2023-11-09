ae3ffe8f40f766a6696fc86025029151c351ca23
Илья <110288384+Ilya-Karpovich@users.noreply.github.com>
2023-11-09 12:28:00 +0300

    remake_9

--- trunk/as0006106/task_01/src/main_12.cpp
+++ trunk/as0006106/task_01/src/main_12.cpp
@@ -8,21 +8,21 @@ class TemperatureController
 	const double B = 0.9;
 	const double C = 1.7;
 	const double D = 1;
-	double time;
 	double temperature;
 	double warm;
 
 
 
 public:
-	TemperatureController(double time=0, double temperature=0, double warm=0) : time(time),temperature(temperature),warm(warm) {
+	TemperatureController(double temperature=0, double warm=0) : temperature(temperature),warm(warm) {
 	}
 	double LinearSimulation() const {
 		double y = A * this->temperature + B * this->warm;
 		return y;
 	}
 	double NonLinearSimulation() const {
-		double start_temperature = 1, start_warm = 1;
+		double start_temperature = 1;
+		double start_warm = 1;
 		double simulation = A * this->temperature - B * pow(start_temperature, 2) + C * this->warm + D * sin(start_warm);
 		return simulation;
 	}
@@ -30,14 +30,13 @@ public:
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	double time, temperature, warm;
+	double temperature;
+	double warm;
 	cout << "Введите температуру:"<<endl;
 	cin >> temperature;
-	cout << "Введите момент времени:" << endl;
-	cin >> time;
 	cout << "Введите количество тепла:" << endl;
 	cin >> warm;
-	TemperatureController tmp(time, temperature, warm);
+	TemperatureController tmp(temperature, warm);
 	cout << "Линейная модель y(t) = " << tmp.LinearSimulation()<<endl;
 	cout << "Нелинейная модель y(t) = " << tmp.NonLinearSimulation()<<endl;
 	system("pause");
