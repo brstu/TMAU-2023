1ffd5b5d21cf24ea589a1a5138ed0f88fa5fba75
Клунин Тимур <klunini2@gmail.com>
2023-11-17 01:44:56 +0300

    sonar fix 3

--- trunk/as0006108/task_01/src/Lab1.cpp
+++ trunk/as0006108/task_01/src/Lab1.cpp
@@ -1,13 +1,18 @@
 ﻿#include <iostream>
 #include <cmath>
 
-const double a = 1, bLin = 1, bNonlin = 0.00001, c = bLin, d = 0.1;
+const double a = 1;
+const double bLin = 1;
+const double bNonlin = 0.00001;
+const double c = bLin;
+const double d = 0.1;
 void calculateLinearModel(int time, double yCurrent, double& inputWarm);
 void calculateNonlinearModel(int time, double yCurrent, double inputWarm);
 int main()
 {
 	setlocale(0, "");
-	double yCurrent = 0.0, inputWarm = 0.0;
+	double yCurrent = 0.0;
+	double inputWarm = 0.0;
 	int time = 0;
 	do
 	{
@@ -34,7 +39,9 @@ void calculateLinearModel(int time, double yCurrent, double& inputWarm)
 
 void calculateNonlinearModel(int time, double yCurrent, double inputWarm)
 {
-	double yNext = 0.0, yPrev = 0.0, inputWarmPrev = 0.0;
+	double yNext = 0.0;
+	double yPrev = 0.0;
+	double inputWarmPrev = 0.0;
 	while (time--)
 	{
 		yNext = (a * yCurrent) - (bNonlin * pow(yPrev, 2)) + (c * inputWarm) + (d * sin(inputWarmPrev));
