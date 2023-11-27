b2b7cc2a3dc48ec4992d061282f565a22418af87
Шалабута Михаил <145102848+Mikhail0Shalabuta@users.noreply.github.com>
2023-11-27 19:48:51 +0300

    task1

---
+++ trunk/as006222/task1/doc/readme.md
@@ -0,0 +1,33 @@
+# Laboratory work №.1              
+<p align="center">Министерство Образования Республики Беларусь</p>
+<p align="center">«Брестский Государственный Технический Университет»</p>       
+<p align="center">Кафедра ИИТ</p>
+<br><br><br><br>
+<p align="center">Лабораторная работа №.1</p>
+<br><br><br>
+<p align="right">Выполнено</p> 
+<p align="right">Студентом 3 Курса</p> 
+<p align="right">Группы AC-62 </p>
+<p align="right">Шалабута Михаил</p>
+<p align="right">Проверено </p> 
+<p align="right">Иванюк Д.С.</p>
+<br><br><br>
+<p align="center">Брест 2023</p>
+
+---
+
+## Задание 1. 
+ 
+Написать программу эмулирующую температуру в Линейном и Нелинейном Режимах
+
+### Реализация
+Имеются
+
+1. ФУНКЦИЯ Линейного вычисления
+2. ФУНКЦИЯ НеЛинейного вычисления
+
+<p align="center" style="font-size:30px;font-weight: bold">
+Результат</p>
+
+<p align="center"><img style='border:4px solid #000000'src="122.png"/></p>
+<p align="center"><img style='border:4px solid #000000'src="123.png"/></p>
---
+++ trunk/as006222/task1/src/tmau1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <math.h>
+
+void Linear(double a, double b, double current_temperature, double input_warm, size_t itterations) {
+
+
+    for (size_t i = 0; i < itterations; i++)
+    {
+        double next = a * current_temperature + b * input_warm;
+        std::cout << next << "\n";
+        current_temperature = next;
+    }
+    std::cout << "end\n";
+
+}
+
+void NonLinear(double a, double b, double c, double d, double current_temperature, double input_warm, size_t itterations) {
+
+    double previous_temperature = 0;
+    for (size_t i = 0; i < itterations; i++)
+    {
+        double next = a * current_temperature - b * previous_temperature * previous_temperature + c * input_warm + d * sin(input_warm);
+        previous_temperature = current_temperature;
+        current_temperature = next;
+        std::cout << next << "\n";
+    }
+    std::cout << "end\n";
+
+
+}
+
+int main()
+{
+    double a, b, c, d, current_temperature, input_warm;
+    bool mode;
+    size_t itterations;
+    std::cout << "A: ";
+    std::cin >> a;
+    std::cout << "B: ";
+    std::cin >> b;
+    std::cout << "C: ";
+    std::cin >> c;
+    std::cout << "D: ";
+    std::cin >> d;
+    std::cout << "Current temperature: ";
+    std::cin >> current_temperature;
+    std::cout << "Input warm: ";
+    std::cin >> input_warm;
+    std::cout << "Mode (1 - linear, 0 - NonLinear): ";
+    std::cin >> mode;
+    std::cout << "How many itterations: ";
+    std::cin >> itterations;
+
+    if (mode)
+        Linear(a, b, current_temperature, input_warm, itterations);
+    else
+        NonLinear(a, b, c, d, current_temperature, input_warm, itterations);
+
+}
