de02fc9ca42646c5fd17da12ef6b7b427b9ff929
Aveerr <144418921+lookingforyoulfy@users.noreply.github.com>
2023-11-29 00:36:48 +0300

    fix

--- trunk/as0006108/task_02/src/Lab2.cpp
+++ trunk/as0006108/task_02/src/Lab2.cpp
@@ -18,16 +18,16 @@
 class AbsModPID
 {
 public:
-   virtual ~AbsModPID() = default;
+virtual ~AbsModPID() = default;
 
-   /**
-   * \brief Виртуальная функция, переопределенная в дочерних классах LinModPID и NonModPID
-   *
-   * Функция переопределяется в дочерних классах и служит для рассчета линейной/нелинейной модели.
-   * \param curTemmmp Температура помещения
-   * \param warmInpuutt Входящее тепло
-   */
-   virtual double calcPIDMod(double curTemmmp, double warmInpuutt) = 0;
+/**
+* \brief Виртуальная функция, переопределенная в дочерних классах LinModPID и NonModPID
+*
+* Функция переопределяется в дочерних классах и служит для рассчета линейной/нелинейной модели.
+* \param curTemmmp Температура помещения
+* \param warmInpuutt Входящее тепло
+*/
+virtual double calcPIDMod(double curTemmmp, double warmInpuutt) = 0;
 };
 
 
@@ -42,38 +42,38 @@ public:
 class NonModPID : public AbsModPID
 {
 private:
-   double a_;   ///< Коэффициент
-   double b_;   ///< Коэффициент
-   double c__;   ///< Коэффициент
-   double d;   ///< Коэффициент
-   double prevY___ = 0; ///< Предыдущая температура 
-   double nextY___;   ///< Получаемая нами температура
-   double prevW__ = 0; ///< Предыдущее тепло
+double a_;   ///< Коэффициент
+double b_;   ///< Коэффициент
+double c__;   ///< Коэффициент
+double d;   ///< Коэффициент
+double prevY___ = 0; ///< Предыдущая температура 
+double nextY___;   ///< Получаемая нами температура
+double prevW__ = 0; ///< Предыдущее тепло
 public:
 
-   NonModPID(double a, double b, double c, double d, double yNext) :
-   a_(a), b_(b), c__(c), d(d), nextY___(yNext)
-   { }
+NonModPID(double a, double b, double c, double d, double yNext) :
+a_(a), b_(b), c__(c), d(d), nextY___(yNext)
+{ }
 
 
-   /**
-   * Переопределённый метод для рассчёта нелинейной модели
-   *
-   * Код:
-   * \code
-   * nextY___ = a_ * curTemmmp - b_ * pow(prevY___, 2) + c__ * warmInpuutt + d * sin(prevW__);
-   * prevY___ = nextY___;
-   * prevW__ = warmInpuutt;
-   * return nextY___;
-   * \endcode
-   */
+/**
+* Переопределённый метод для рассчёта нелинейной модели
+*
+* Код:
+* \code
+* nextY___ = a_ * curTemmmp - b_ * pow(prevY___, 2) + c__ * warmInpuutt + d * sin(prevW__);
+* prevY___ = nextY___;
+* prevW__ = warmInpuutt;
+* return nextY___;
+* \endcode
+*/
 
-   double calcPIDMod(double curTemmmp, double warmInpuutt) override{
-   nextY___ = a_ * curTemmmp - b_ * pow(prevY___, 2) + c__ * warmInpuutt + d * sin(prevW__);
-   prevY___ = curTemmmp;
-   prevW__ = warmInpuutt;
-   return nextY___;
-   }
+double calcPIDMod(double curTemmmp, double warmInpuutt) override{
+nextY___ = a_ * curTemmmp - b_ * pow(prevY___, 2) + c__ * warmInpuutt + d * sin(prevW__);
+prevY___ = curTemmmp;
+prevW__ = warmInpuutt;
+return nextY___;
+}
 };
 
 
@@ -87,29 +87,29 @@ class LinModPID : public AbsModPID
 {
 private:
 
-   double a_;   ///< Коэффициент
-   double b_;   ///< Коэффициент
-   double nextY___; ///< Получаемая нами температура
+double a_;   ///< Коэффициент
+double b_;   ///< Коэффициент
+double nextY___; ///< Получаемая нами температура
 
 public:
-   LinModPID(double a, double b, double yNext) :
-   a_(a), b_(b), nextY___(yNext)
-   { }
+LinModPID(double a, double b, double yNext) :
+a_(a), b_(b), nextY___(yNext)
+{ }
 
-   /**
-   * Переопределённый метод для рассчёта линейной модели
-   *
-   * Код:
-   * \code
-   * nextY___ = a_ * curTemmmp + b_ * warmInpuutt;
-   * return nextY___;
-   * \endcode
-   */
+/**
+* Переопределённый метод для рассчёта линейной модели
+*
+* Код:
+* \code
+* nextY___ = a_ * curTemmmp + b_ * warmInpuutt;
+* return nextY___;
+* \endcode
+*/
 
-   double calcPIDMod(double curTemmmp, double warmInpuutt) override{
-   nextY___ = a_ * curTemmmp + b_ * warmInpuutt;
-   return nextY___;
-   }
+double calcPIDMod(double curTemmmp, double warmInpuutt) override{
+nextY___ = a_ * curTemmmp + b_ * warmInpuutt;
+return nextY___;
+}
 };
 
 /**
@@ -123,177 +123,177 @@ class RegulPID
 {
 private:
 
-   double const_int_;   ///< Постоянная интегрирования
-   double step_kvant_;   ///< Шаг для квантования
-   double const_difer_;   ///< Постоянная дифференцирования
-   double koev_peredach_;   ///< Коэффициент передачи
-   double setpoint_ = 0; ///< Текущее значение управляющего воздействия
+double const_int_;   ///< Постоянная интегрирования
+double step_kvant_;   ///< Шаг для квантования
+double const_difer_;   ///< Постоянная дифференцирования
+double koev_peredach_;   ///< Коэффициент передачи
+double setpoint_ = 0; ///< Текущее значение управляющего воздействия
 
-   double calc__uk(double ekb_1, double ekb_2, double ekb_3){
-   /**
-   * \brief Метод для рассчёта setpoint_
-   *
-   * Метод рассчёта текущего значения управляющего воздействия на объект управления
-   *
-   * Код:
-   * \code
-   * 
-   * double q1 = koev_peredach_ * (1 + const_difer_ / step_kvant_);
-   * double q2 = -koev_peredach_ * (1 + 2 * const_difer_ / step_kvant_ - step_kvant_ / const_int_);
-   * double q3 = koev_peredach_ * const_difer_ / step_kvant_;
-   * setpoint_ += (q1 * ekb_1 + q2 * ekb_2 + q3 * ekb_3);
-   * 
-   * return setpoint_;
-   * \endcode
-   */
+double calc__uk(double eeeeee_1, double eeeeee_2, double eeeeee_3){
+/**
+* \brief Метод для рассчёта setpoint_
+*
+* Метод рассчёта текущего значения управляющего воздействия на объект управления
+*
+* Код:
+* \code
+* 
+* double q1 = koev_peredach_ * (1 + const_difer_ / step_kvant_);
+* double q2 = -koev_peredach_ * (1 + 2 * const_difer_ / step_kvant_ - step_kvant_ / const_int_);
+* double q3 = koev_peredach_ * const_difer_ / step_kvant_;
+* setpoint_ += (q1 * eeeeee_1 + q2 * eeeeee_2 + q3 * eeeeee_3);
+* 
+* return setpoint_;
+* \endcode
+*/
 
-   double q1 = koev_peredach_ * (1 + const_difer_ / step_kvant_); /// q1 - Параметр регулятора
-   double q2 = -koev_peredach_ * (1 + 2 * const_difer_ / step_kvant_ - step_kvant_ / const_int_); /// q2 - Параметр регулятора
-   double q3 = koev_peredach_ * const_difer_ / step_kvant_; /// q3 - Параметр регулятора
-   setpoint_ += (q1 * ekb_1 + q2 * ekb_2 + q3 * ekb_3);
+double q1 = koev_peredach_ * (1 + const_difer_ / step_kvant_); /// q1 - Параметр регулятора
+double q2 = -koev_peredach_ * (1 + 2 * const_difer_ / step_kvant_ - step_kvant_ / const_int_); /// q2 - Параметр регулятора
+double q3 = koev_peredach_ * const_difer_ / step_kvant_; /// q3 - Параметр регулятора
+setpoint_ += (q1 * eeeeee_1 + q2 * eeeeee_2 + q3 * eeeeee_3);
 
-   return setpoint_;
-   }
+return setpoint_;
+}
 
 public:
 
-   RegulPID(double T, double T0, double TD, double K) :
-   const_int_(T),step_kvant_(T0), const_difer_(TD),koev_peredach_(K)
-   { }
+RegulPID(double T, double T0, double TD, double K) :
+const_int_(T),step_kvant_(T0), const_difer_(TD),koev_peredach_(K)
+{ }
 
-   void PIDCalcWrit(double need, double start){
-   /**
-   * \brief Метод моделирования ПИД-регулятора
-   *
-   * В данном методе мы моделируем ПИД-регулятор и записываем результаты в файл results.txt
-   * \param need Желаемое значение
-   * \param start Стартовое значение y
-   *
-   * Код:
-   * \code
-   * std::ofstream fout("results.txt");
-   * 
-   * if (fout){
-   *   double ekb_1 = 0;
-   *   double ekb_2 = 0;
-   *   double ekb_3 = 0;
-   *   double y = start;
-   *   double u = 0;
-   * 
-   *   LinModPID linear(0.333, 0.667, 1);
-   * 
-   *   fout << "Линейная модель: " << std::endl;
-   * 
-   *   for (int i = 0; i < 50; ++i){
-   * 
-   *   ekb_1 = need - y;
-   *   u = calc__uk(ekb_1, ekb_2, ekb_3);
-   *   y = linear.calcPIDMod(start, u);
-   *   fout << "E=" << ekb_1 << " Y=" << y << " U=" << u << std::endl;
-   * 
-   *   ekb_3 = ekb_2;
-   *   ekb_2 = ekb_1;
-   *   }
-   * 
-   *   ekb_2 = 0;
-   *   ekb_3 = 0;
-   * 
-   *   y = start;
-   *   setpoint_ = 0;
-   * 
-   *   fout << "Нелинейная модель: " << std::endl;
-   * 
-   *   NonModPID nonLinear(1, 0.0033, 0.525, 0.525, 1);
-   *   for (int i = 0; i < 50; ++i){
-   *   ekb_1 = need - y;
-   *   u = calc__uk(ekb_1, ekb_2, ekb_3);
-   *   y = nonLinear.calcPIDMod(start, u);
-   *   fout << "E=" << ekb_1 << " Y=" << y << " U=" << u << std::endl;
-   * 
-   *   ekb_3 = ekb_2;
-   *   ekb_2 = ekb_1;
-   *   }
-   * }
-   * else{
-   *   std::cout << "Не удалось открыть файл для записи результатов." << std::endl;
-   * }
-   * \endcode
-   */
+void PIDCalcWrit(double need, double start){
+/**
+* \brief Метод моделирования ПИД-регулятора
+*
+* В данном методе мы моделируем ПИД-регулятор и записываем результаты в файл results.txt
+* \param need Желаемое значение
+* \param start Стартовое значение y
+*
+* Код:
+* \code
+* std::ofstream fout("results.txt");
+* 
+* if (fout){
+*   double eeeeee_1 = 0;
+*   double eeeeee_2 = 0;
+*   double eeeeee_3 = 0;
+*   double y = start;
+*   double u = 0;
+* 
+*   LinModPID linear(0.333, 0.667, 1);
+* 
+*   fout << "Линейная модель: " << std::endl;
+* 
+*   for (int i = 0; i < 50; ++i){
+* 
+*   eeeeee_1 = need - y;
+*   u = calc__uk(eeeeee_1, eeeeee_2, eeeeee_3);
+*   y = linear.calcPIDMod(start, u);
+*   fout << "E=" << eeeeee_1 << " Y=" << y << " U=" << u << std::endl;
+* 
+*   eeeeee_3 = eeeeee_2;
+*   eeeeee_2 = eeeeee_1;
+*   }
+* 
+*   eeeeee_2 = 0;
+*   eeeeee_3 = 0;
+* 
+*   y = start;
+*   setpoint_ = 0;
+* 
+*   fout << "Нелинейная модель: " << std::endl;
+* 
+*   NonModPID nonLinear(1, 0.0033, 0.525, 0.525, 1);
+*   for (int i = 0; i < 50; ++i){
+*   eeeeee_1 = need - y;
+*   u = calc__uk(eeeeee_1, eeeeee_2, eeeeee_3);
+*   y = nonLinear.calcPIDMod(start, u);
+*   fout << "E=" << eeeeee_1 << " Y=" << y << " U=" << u << std::endl;
+* 
+*   eeeeee_3 = eeeeee_2;
+*   eeeeee_2 = eeeeee_1;
+*   }
+* }
+* else{
+*   std::cout << "Не удалось открыть файл для записи результатов." << std::endl;
+* }
+* \endcode
+*/
 
-   std::ofstream fout("results.txt");
+std::ofstream fout("results.txt");
 
-   if (fout){
-   double ekb_1;
-   double ekb_2 = 0;
-   double ekb_3 = 0;
-   double y = start;
-   double u;
+if (fout){
+double eeeeee_1;
+double eeeeee_2 = 0;
+double eeeeee_3 = 0;
+double y = start;
+double u;
 
-   LinModPID linear(0.333, 0.667, 1);
+LinModPID linear(0.333, 0.667, 1);
 
-   fout << "Линейная модель: " << std::endl;
+fout << "Линейная модель: " << std::endl;
+
+for (int i = 0; i < 50; ++i){
+eeeeee_1 = need - y;
+
+u = calc__uk(eeeeee_1, eeeeee_2, eeeeee_3);
+y = linear.calcPIDMod(start, u);
+
+fout << "E=" << eeeeee_1 << " Y=" << y << " U=" << u << std::endl;
+
+eeeeee_3 = eeeeee_2;
+eeeeee_2 = eeeeee_1;
+}
+
+eeeeee_2 = 0;
+eeeeee_3 = 0;
+y = start;
+setpoint_ = 0;
+
+fout << "Нелинейная модель: " << std::endl;
+
+NonModPID nonLinear(1, 0.0033, 0.525, 0.525, 1);
 
    for (int i = 0; i < 50; ++i){
-   ekb_1 = need - y;
+      eeeeee_1 = need - y;
 
-   u = calc__uk(ekb_1, ekb_2, ekb_3);
-   y = linear.calcPIDMod(start, u);
+      u = calc__uk(eeeeee_1, eeeeee_2, eeeeee_3);
+      y = nonLinear.calcPIDMod(start, u);
 
-   fout << "E=" << ekb_1 << " Y=" << y << " U=" << u << std::endl;
+      fout << "E=" << eeeeee_1 << " Y=" << y << " U=" << u << std::endl;
 
-   ekb_3 = ekb_2;
-   ekb_2 = ekb_1;
+      eeeeee_3 = eeeeee_2;
+      eeeeee_2 = eeeeee_1;
    }
-
-   ekb_2 = 0;
-   ekb_3 = 0;
-   y = start;
-   setpoint_ = 0;
-
-   fout << "Нелинейная модель: " << std::endl;
-
-   NonModPID nonLinear(1, 0.0033, 0.525, 0.525, 1);
-
-   for (int i = 0; i < 50; ++i){
-   ekb_1 = need - y;
-
-   u = calc__uk(ekb_1, ekb_2, ekb_3);
-   y = nonLinear.calcPIDMod(start, u);
-
-   fout << "E=" << ekb_1 << " Y=" << y << " U=" << u << std::endl;
-   
-   ekb_3 = ekb_2;
-   ekb_2 = ekb_1;
-   }
-   }
-   else{
-   std::cout << "Не удалось открыть файл для записи результатов." << std::endl;
-   }
-   }
+}
+else{
+std::cout << "Не удалось открыть файл для записи результатов." << std::endl;
+}
+}
 };
 
 int main(){
-   /**
-   * \brief Главная функция main
-   *
-   * Создаём объект класса RegulPID и вызываем метод для рассчёта и записи результатов в файл
-   *
-   * Код:
-   * \code
-   * setlocale(0, "");
-   * RegulPID object(10,10,50,0.1);
-   * object.PIDCalcWrit(5, 2);
-   * std::cout << "Данные были сохранены в файл results.txt" << std::endl;
-   * return 0;
-   * \endcode
-   */
-   setlocale(0, "");
+/**
+* \brief Главная функция main
+*
+* Создаём объект класса RegulPID и вызываем метод для рассчёта и записи результатов в файл
+*
+* Код:
+* \code
+* setlocale(0, "");
+* RegulPID object(10,10,50,0.1);
+* object.PIDCalcWrit(5, 2);
+* std::cout << "Данные были сохранены в файл results.txt" << std::endl;
+* return 0;
+* \endcode
+*/
+setlocale(0, "");
 
-   RegulPID object(10, 10, 40, 0.1);
+RegulPID object(10, 10, 40, 0.1);
 
-   object.PIDCalcWrit(5, 2);
+object.PIDCalcWrit(5, 2);
 
-   std::cout << "Данные были сохранены в файл results.txt" << std::endl;
+std::cout << "Данные были сохранены в файл results.txt" << std::endl;
 
-   return 0;
+return 0;
 }
